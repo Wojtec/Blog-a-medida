fe07b2e95e3c85d4126a5d6696acdf37cf7b84dd
AxelBrinck <axel.louis.1991@gmail.com>
2020-01-08 14:42:48 +0100

    added relations to database creation script

--- database.sql
+++ database.sql
@@ -3,6 +3,19 @@ create database if not exists blog;
 
 use blog;
 
+create table categories (
+	category_id		int				not null	auto_increment,
+	category_name	varchar(16)		not null	unique,
+	primary key (category_id)
+);
+
+create table users (
+	user_id			int				not null	auto_increment,
+	email			varchar(30)		not null	unique,
+    pass			varchar(30)		not null,
+    primary key (user_id)
+);
+
 create table posts (
 	post_id			int				not null 	auto_increment,
     user_id			int				not null,
@@ -11,43 +24,18 @@ create table posts (
     title			varchar(20)		not null,
     content			varchar(1000)	not null,
     is_published	bool			not null,
-    primary key (post_id)
-);
-
-create table users (
-	user_id			int				not null	auto_increment,
-	email			varchar(30)		not null,
-    pass			varchar(30)		not null,
-    primary key (user_id)
-);
-
-create table post_user (
-	post_id			int				not null,
-    user_id			int				not null,
-    primary key (post_id, user_id)
-);
-
-create table categories (
-	category_id		int				not null	auto_increment,
-	category_name	varchar(16)		not null,
-	primary key (category_id)
+    primary key (post_id),
+    foreign key (user_id) references users(user_id) on delete cascade,
+    foreign key (category_id) references categories(category_id) on delete cascade
 );
 
 create table comments (
 	comment_id		int				not null	auto_increment,
-    user_id			int				not null,	
-    comment_text	varchar(200)	not null,
-    primary key (comment_id)
-);
-
-create table commet_post (
-	comment_id		int				not null,
-    post_id			int				not null,
-    primary key (comment_id, post_id)
-);
-
-create table comment_user (
-	comment_id		int				not null,
     user_id			int				not null,
-    primary key (comment_id, user_id)
+    post_id			int				not null,
+    comment_text	varchar(200)	not null,
+    comment_date	datetime		not null,
+    primary key (comment_id),
+    foreign key (user_id) references users(user_id) on delete cascade,
+    foreign key (post_id) references posts(post_id) on delete cascade
 );
