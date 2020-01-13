0f6c69163c71094f37fdc88b5596230499d27d18
AxelBrinck <axel.louis.1991@gmail.com>
2020-01-13 09:51:07 +0100

    corrected config file

--- config/config.php
+++ config/config.php
@@ -1,17 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 define('URL', 'http://localhost/Blog-a-medida/');
-=======
-define('URL', 'http://localhost/curso/43.%20MVC-02/');
 
 //Axel laptop configuration
-<<<<<<< HEAD
-/*
->>>>>>> e9d75b6b32f0c525903e8f8fae50c0fe2f8e7a50
-=======
-
->>>>>>> 798c506da3906d2a21930cf6ae2c7a6d8da88c01
 define('HOST', 'localhost');
 define('DB', 'blog');
 define('USER', 'root');
