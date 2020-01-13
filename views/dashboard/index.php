03b4ad0b8fcf2ea9e56e470ccbdb57a9629a88bc
AxelBrinck <axel.louis.1991@gmail.com>
2020-01-13 10:01:52 +0100

    clean commit ready to check login credentials

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -11,17 +11,12 @@
     <title>Blog</title>
 </head>
 <body>
+
     <?php require 'views/header.php'; ?>
-    <!-- left-navbar -->
-    <section class="section-nav">
-<div class="navbar">
     
-    <div class="category-list"><h4>Category </h4><hr></div>
-</div>
     <h1>dashboard!</h1>
 
     <?php require 'views/footer.php'; ?>
-    <script src="Other/login.js"></script>
-
+    
 </body>
 </html>
