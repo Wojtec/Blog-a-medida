5130564384f917fa02849d7caf86dda1ea509c0b
AxelBrinck <axel.louis.1991@gmail.com>
2020-01-14 09:33:04 +0100

    database now holds tags for posts

--- Other/database.sql
+++ Other/database.sql
@@ -21,10 +21,11 @@ create table posts (
 	post_id			int				not null 	auto_increment,
     user_id			int				not null,
     category_id		int				not null,
-    publish_date	datetime 		not null,
+    publish_date	datetime 		not null	default now(),
     title			varchar(100)	not null,
     content			varchar(2000)	not null,
     is_public		bool			not null,
+    tags			varchar(1000)	not null	default '',
     primary key (post_id),
     foreign key (user_id) references users(user_id) on delete cascade,
     foreign key (category_id) references categories(category_id) on delete cascade
@@ -35,7 +36,7 @@ create table comments (
     user_id			int,
     post_id			int				not null,
     comment_text	varchar(200)	not null,
-    comment_date	datetime		not null,
+    comment_date	datetime		default now(),
     primary key (comment_id),
     foreign key (user_id) references users(user_id) on delete cascade,
     foreign key (post_id) references posts(post_id) on delete cascade
