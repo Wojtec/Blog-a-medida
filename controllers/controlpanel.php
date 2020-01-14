5945b4168d234d69ba95be712e019bffaed8d57a
Wojtec <poninski.w@gmail.com>
2020-01-14 10:44:01 +0100

    start date

--- controllers/controlpanel.php
+++ controllers/controlpanel.php
@@ -18,14 +18,14 @@ class controlpanel extends controller
             header("Location: " . constant("URL") . "login");
         }
 
-        if(isset($_SESSION['user_id']) && isset($_POST['new-category']) && isset($_POST['tags']) && isset($_POST['new-title']) && isset($_POST['post-message']))
+        if(isset($_SESSION['user_id']) && isset($_POST['new-category']) && isset($_POST['tags']) && isset($_POST['new-title']) && isset($_POST['post-message'])&& isset($_POST['start-date']))
         {
             $user_id = $_SESSION['user_id'];
             $new_category = $_POST['new-category'];
             $new_title = $_POST['new-title'];
             $new_post_message = $_POST['post-message'];
             $tags = $_POST['tags'];
-
+            $start_date = $_POST['start-date'];
 
 
             $post = new post();
@@ -34,7 +34,7 @@ class controlpanel extends controller
             $post->tags = $tags;
             $post->title = $new_title;
             $post->content = $new_post_message;
-            $post->publish_date = date("Y-m-d H:i:s");
+            $post->publish_date = $start_date;
             $post->is_public = true;
 
             $this->model->insertPost($post);
