df18f59924e44956629663776987dea104401897
AxelBrinck <axel.louis.1991@gmail.com>
2020-01-14 18:04:58 +0100

    login and register styled

--- views/register/index.php
+++ views/register/index.php
@@ -4,27 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="public/reset.css">
-
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="public/styles/reset.css">
+    <link rel="stylesheet" href="public/styles/base-style.css">
+    <link rel="stylesheet" href="public/styles/full-page-form.css">
     <title>Blog</title>
 </head>
 <body>
 
     <form action="<?php echo constant('URL'); ?>register/register" method="post">
-        <p>Username</p>
-        <input type="text" name="username" required>
-        <p>Email</p>
-        <input type="email" name="email" required>
-        <p>Password</p>
-        <input type="password" name="password" required>
-        <p>Repeat password</p>
-        <input type="password" name="password_confirmation" required>
-        <button type="submit">Submit</button>
+        <div>
+            <input type="text" placeholder="Username" name="username" required>
+            <input type="email" placeholder="Email" name="email" required>
+            <input type="password" placeholder="Password" name="password" required>
+            <input type="password" placeholder="Repeat password" name="password_confirmation" required>
+            <button type="submit">Submit</button>
+            <div>
+                <?php 
+                    echo '<p>You already have an account? <a href="' . constant("URL") . 'login">login</a></p>';
+                ?>
+            </div>
+        </div>
+        
     </form>
     
-    <?php 
-        echo '<p>You already have an account? <a href="' . constant("URL") . 'login">login</a></p>';
-    ?>
 
 </body>
 </html>
